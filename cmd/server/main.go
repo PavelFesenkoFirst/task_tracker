817a550dd4860ea97a3bd03b64cc675c203562b1
FesenkoPavlo <pavlo@ice.co.uk>
2025-10-08 13:12:53 -0700

    init main file

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,27 @@
-package server
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"status": "ok",
+		})
+	})
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file, proceeding with system environment variables")
+	}
+
+	port := os.Getenv("APP_PORT")
+	r.Run(":" + port)
+}
